c2bbc9549fd15fae66944453a77db05983afe6ed
Gregor Fabritius <fabritius@grefab.de>
2020-01-26 05:03:44 +0100

    fixed context handling. removed unused connection termination logic.

--- lib/client.go
+++ lib/client.go
@@ -9,13 +9,11 @@ import (
 
 type remoteDisplay struct {
 	display pb.DisplayClient
-	ctx     context.Context
-	Close   context.CancelFunc
 }
 
 // Creates a remoteDisplay. A remoteDisplay should be treated statically and usually lives through
 // the entire program runtime. Its resources are automatically cleaned up by the OS after program
-// termination. In rare cases, use the provided Close method to perform a manual cleanup.
+// termination.
 func NewRemoteDisplay(serverAddr string) *remoteDisplay {
 	// prepare gRPC
 	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
@@ -23,22 +21,14 @@ func NewRemoteDisplay(serverAddr string) *remoteDisplay {
 		panic(err)
 	}
 	storageClient := pb.NewDisplayClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 
 	return &remoteDisplay{
 		display: storageClient,
-		ctx:     ctx,
-		Close: func() {
-			cancel()
-			err := conn.Close()
-			if err != nil {
-				panic(err)
-			}
-		},
 	}
 }
 
 func (c *remoteDisplay) ShowFrame(frame *pb.Frame) (err error) {
-	_, err = c.display.ShowFrame(c.ctx, frame)
+	ctx, _ := context.WithTimeout(context.Background(), 60*time.Second)
+	_, err = c.display.ShowFrame(ctx, frame)
 	return err
 }
