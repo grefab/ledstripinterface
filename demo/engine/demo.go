24f7b1a6d548e6062810fcba5bb0fbce16866b51
Gregor Fabritius <fabritius@grefab.de>
2020-01-31 02:42:10 +0100

    played with different animations

--- demo/engine/demo.go
+++ demo/engine/demo.go
@@ -3,33 +3,45 @@ package engine
 import (
 	pb "ledstripinterface/pb"
 	"log"
+	"math/rand"
+	"sync"
 	"time"
 )
 
 func Play(render func(frame pb.Frame) error) {
-	stateUpdates := make(chan state, 10)
+	var lastState state
+	m := sync.Mutex{}
+
 	go func() {
 		state := NewState(216)
+		transmitTicker := time.NewTicker(time.Second / 100)
 		for {
 			select {
-			case <-time.After(time.Millisecond * 10):
+			case <-transmitTicker.C:
 				state.Update()
-				stateUpdates <- *state
+				m.Lock()
+				lastState = *state
+				m.Unlock()
 			}
 		}
 	}()
 
-	var lastState state
+	const frameDuration = time.Second / 100 // Hz
 	for {
-		select {
-		case state := <-stateUpdates:
-			lastState = state
-		case <-time.After(time.Millisecond * 10):
-			frame := lastState.ToFrame()
-			err := render(frame)
-			if err != nil {
-				log.Print(err)
-			}
+		startTime := time.Now()
+		m.Lock()
+		frame := lastState.ToFrame()
+		m.Unlock()
+		err := render(frame)
+		if err != nil {
+			log.Print(err)
+		}
+		duration := time.Now().Sub(startTime)
+		pause := frameDuration - duration
+		if pause <= 0 {
+			log.Printf("render overflow: %v", pause)
+		} else {
+			time.Sleep(pause)
 		}
 	}
 }
@@ -39,38 +51,79 @@ type state struct {
 	upwards    bool
 	minimum    float32
 	maximum    float32
+	timePassed time.Duration
 	pixels     []float32
+	backBuffer []float32
 }
 
 func NewState(nLed int) *state {
+	makeCenter := func() []float32 {
+		a := make([]float32, nLed)
+		for i := range a {
+			a[i] = 0.5
+		}
+		return a
+	}
 	return &state{
 		brightness: 0.5,
 		upwards:    true,
 		minimum:    0.3,
 		maximum:    1.0,
-		pixels:     make([]float32, nLed),
+		timePassed: 0,
+		pixels:     makeCenter(),
+		backBuffer: makeCenter(),
 	}
 }
 
 func (s *state) Update() {
-	if s.brightness >= s.maximum {
-		s.upwards = false
-	}
-	if s.brightness <= s.minimum {
-		s.upwards = true
-	}
-	if s.upwards {
-		s.brightness += 0.01
-	} else {
-		s.brightness -= 0.01
-	}
+	/*
+		// global breathe
+		if s.brightness >= s.maximum {
+			s.upwards = false
+		}
+		if s.brightness <= s.minimum {
+			s.upwards = true
+		}
+		if s.upwards {
+			s.brightness += 0.01
+		} else {
+			s.brightness -= 0.01
+		}
 
+		for i := range s.pixels {
+			s.pixels[i] = s.brightness
+		}
+	*/
+
+	/*
+		// randomly distribute
+		s.pixels[rand.Int()%len(s.pixels)] = s.brightness
+	*/
+	/*
+		// sparkle
+		for i := range s.pixels {
+			s.pixels[i] = 0
+		}
+		for i := 0; i < len(s.pixels) / 10; i++ {
+			s.pixels[rand.Int()%len(s.pixels)] = 1
+		}
+	*/
+
+	// random glow
+	s.timePassed += time.Millisecond * 10
+	if s.timePassed > time.Second {
+		s.timePassed = 0
+		const size = 10
+		for i := size / 2; i < len(s.pixels)-size/2; i += size {
+			dst := s.minimum + (rand.Float32() / (s.maximum + s.minimum))
+			for j := i - size/2; j < i+size/2; j++ {
+				s.backBuffer[j] = dst
+			}
+		}
+	}
 	for i := range s.pixels {
-		s.pixels[i] = s.brightness
+		s.pixels[i] += (s.backBuffer[i] - s.pixels[i]) / 50
 	}
-
-	// randomly distribute
-	// s.pixels[rand.Int()%len(s.pixels)] = s.brightness
 }
 
 func (s *state) ToFrame() pb.Frame {
