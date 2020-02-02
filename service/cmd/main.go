56aeb61197e81d4eca71e9990ef953c801c2f382
Gregor Fabritius <fabritius@grefab.de>
2020-02-02 15:20:10 +0100

    cleanup

--- service/cmd/main.go
+++ service/cmd/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"flag"
 	"ledstripinterface/demo"
-	"ledstripinterface/grpc"
 	pb "ledstripinterface/proto"
-	"ledstripinterface/serial"
+	"ledstripinterface/service"
 	"ledstripinterface/udp"
 	"log"
 )
@@ -14,21 +13,27 @@ func main() {
 	log.SetFlags(log.LUTC | log.Ldate | log.Ltime | log.Lmicroseconds)
 	clientMode := flag.Bool("client", false, "start in client mode, tests connection to server, needs endpoint")
 	endpoint := flag.String("endpoint", "0.0.0.0:15050", "endpoint to listen to in server mode or to connect to in client mode")
-	serialPort := flag.String("updTarget", "10.13.37.10:1337", "IP and port for UDP target")
+	udpTarget := flag.String("updTarget", "10.13.37.10:1337", "IP and port for UDP target")
 	flag.Parse()
 
 	if *clientMode {
 		log.Print("running in client mode")
-		display := grpc.NewRemoteDisplay(*endpoint)
+		display := service.NewRemoteDisplay(*endpoint)
 		demo.PlayShiftRegisterDemo(func(frame pb.Frame) error {
 			return display.ShowFrame(&frame)
 		})
 	} else {
 		log.Print("running in server mode. see -help for other options")
-		ip, port := grpc.ParseEndpoint(*endpoint)
-		log.Printf("gRPC to UPD on arduino: %v, running gRPC server on %v:%v", *serialPort, ip, port)
-		grpc.RunServer(ip, port, func(frame pb.Frame) {
-			err := udp.Send(*serialPort, serial.FrameToBytes(frame))
+		ip, port := service.ParseEndpoint(*endpoint)
+		log.Printf("gRPC to UPD on arduino: %v, running gRPC server on %v:%v", *udpTarget, ip, port)
+		service.RunServer(ip, port, func(frame pb.Frame) {
+			data := make([]byte, 0, len(frame.Pixels)*3)
+			for _, color := range frame.Pixels {
+				data = append(data, byte(color.R))
+				data = append(data, byte(color.G))
+				data = append(data, byte(color.B))
+			}
+			err := udp.Send(*udpTarget, data)
 			if err != nil {
 				log.Println(err)
 			}
